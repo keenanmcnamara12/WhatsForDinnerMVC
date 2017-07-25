e057056002ea48086137ac5a1c88d3d10d4a9640
skrepsky <skrepsky@EPIC33735>
2017-07-25 16:12:09 -0500

    Fixing issue with missing else statement.

--- WhatsForDinnerMVC/Controllers/LoginController.cs
+++ WhatsForDinnerMVC/Controllers/LoginController.cs
@@ -7,48 +7,50 @@ using WhatsForDinnerMVC.Models;
 
 namespace WhatsForDinnerMVC.Controllers
 {
-    public class LoginController : Controller
-    {
-        public ActionResult Index()
-        {
-            return View();
-        }
+	public class LoginController : Controller
+	{
+		public ActionResult Index()
+		{
+			return View();
+		}
 
-        [HttpPost]
-        public ActionResult SubmitAction(FormCollection collection)
-        {
-            var user = new User(collection["email"], collection["password"]);
-            // If valid user, direct to the menu page!
-            if (user.IsValid)
-            {
-                ViewBag.Message = "";
-                this.Session["User"] = user;  // Save the user object so the search page can recover later.
-                return RedirectToAction("index", "Search");
-            }
-            
-            // Not valid login - reload the login page and display viewmessage. 
-            ViewBag.Message = "Unable to login.";
-            return View("Index");
-        }
+		[HttpPost]
+		public ActionResult SubmitAction(FormCollection collection)
+		{
+			var user = new User(collection["email"], collection["password"]);
+			// If valid user, direct to the menu page!
+			if (user.IsValid)
+			{
+				ViewBag.Message = "";
+				this.Session["User"] = user;  // Save the user object so the search page can recover later.
+				return RedirectToAction("index", "Search");
+			}
 
-        public ActionResult Register()
-        {
-            return View("Register");
-        }
+			// Not valid login - reload the login page and display viewmessage. 
+			ViewBag.Message = "Unable to login.";
+			return View("Index");
+		}
 
-        [HttpPost]
-        public ActionResult SubmitRegister(FormCollection collection)
-        {
-            var user = new User(collection["email"], collection["name"],collection["password"]);
-            if (user.CreateNewUser())
-            {
-                ViewBag.Message = "Succesfully created account. You can login now!";
-                return View("Index");
-            }
+		public ActionResult Register()
+		{
+			return View("Register");
+		}
 
-            // On fail, set a fail view message and direct user to the register page.
-            ViewBag.Message = "Username already taken - failed to create account.";
-            return View("Register");
-        }
-    }
+		[HttpPost]
+		public ActionResult SubmitRegister(FormCollection collection)
+		{
+			var user = new User(collection["email"], collection["name"], collection["password"]);
+			if (user.CreateNewUser())
+			{
+				ViewBag.Message = "Succesfully created account. You can login now!";
+				return View("Index");
+			}
+			else
+			{
+				// On fail, set a fail view message and direct user to the register page.
+				ViewBag.Message = "Username already taken - failed to create account.";
+				return View("Register");
+			}
+		}
+	}
 }

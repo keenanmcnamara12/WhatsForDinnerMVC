8d568f30b327df8d6c622dd830a332392f4110c2
skrepsky <skrepsky@EPIC33735>
2017-07-29 19:45:08 -0500

    Create ingredient model and outline for modal popup for recipe

--- WhatsForDinnerMVC/Controllers/MenuListController.cs
+++ WhatsForDinnerMVC/Controllers/MenuListController.cs
@@ -8,53 +8,53 @@ using WhatsForDinnerMVC.Models;
 
 namespace WhatsForDinnerMVC.Controllers
 {
-    public class MenuListController : Controller
-    {
-        // GET: Search
-        public ActionResult Index()
-        {
-            // Get user object from the session, make sure valid.
-            User user = (User)this.Session["user"];
-            if (user == null || !user.IsValid)
-            {
-                return RedirectToAction("index", "Login");
-            }
-            return View("index",user);
-        }
+	public class MenuListController : Controller
+	{
+		// GET: Search
+		public ActionResult Index()
+		{
+			// Get user object from the session, make sure valid.
+			User user = (User)this.Session["user"];
+			if (user == null || !user.IsValid)
+			{
+				return RedirectToAction("index", "Login");
+			}
+			return View("index", user);
+		}
 
-        [HttpPost]
-        public ActionResult NewMenuName(FormCollection collection)
-        {
-            string newMenuName = collection["newMenuName"];
-            // Make sure the user create a manu name!
-            if (String.IsNullOrWhiteSpace(newMenuName))
-            {
-                return RedirectToAction("index", "Search");
-            }
+		[HttpPost]
+		public ActionResult NewMenuName(FormCollection collection)
+		{
+			string newMenuName = collection["newMenuName"];
+			// Make sure the user create a manu name!
+			if (String.IsNullOrWhiteSpace(newMenuName))
+			{
+				return RedirectToAction("index", "Search");
+			}
 
-            // create a new menu record and make it the selected menu
-            User user = (User)Session["user"];
-            int newMenuId = user.AddNewMenu(newMenuName);
-            user.UpdateSelectedMenu(newMenuId);
-            Session["user"] = user;
+			// create a new menu record and make it the selected menu
+			User user = (User)Session["user"];
+			int newMenuId = user.AddNewMenu(newMenuName);
+			user.UpdateSelectedMenu(newMenuId);
+			Session["user"] = user;
 
-            return RedirectToAction("index", "MenuEdit");
-        }
+			return RedirectToAction("index", "MenuEdit");
+		}
 
-        [HttpPost]
-        public ActionResult EditRecipe()
-        {
-            return RedirectToAction("index","MenuEdit");
-        }
+		[HttpPost]
+		public ActionResult EditRecipe()
+		{
+			return RedirectToAction("index", "MenuEdit");
+		}
 
-        [HttpPost]
-        public JsonResult SelectionChanged(int id)
-        {
-            User user = (User)this.Session["user"];
-            user.UpdateSelectedMenu(id);
-            Session["user"] = user;
-            return null;
-        }
+		[HttpPost]
+		public JsonResult SelectionChanged(int id)
+		{
+			User user = (User)this.Session["user"];
+			user.UpdateSelectedMenu(id);
+			Session["user"] = user;
+			return null;
+		}
 
-    }
+	}
 }
